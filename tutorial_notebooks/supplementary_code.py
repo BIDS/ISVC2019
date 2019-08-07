fbc1b0bc9ba43b554cf5b0ae6cc1902f37be8cd5
Alexandre de Siqueira <alex.desiqueira@igdore.org>
2019-08-07 10:30:21 -0700

    Fixing stuff

--- tutorial_notebooks/supplementary_code.py
+++ tutorial_notebooks/supplementary_code.py
@@ -6,6 +6,8 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 
 def show_plane(axis, plane, cmap="gray", title=None):
+    """Shows a specific plane within 3D data.
+    """
     axis.imshow(plane, cmap=cmap)
     axis.set_xticks([])
     axis.set_yticks([])
@@ -17,13 +19,13 @@ def show_plane(axis, plane, cmap="gray", title=None):
 
 
 def slice_in_3D(axis, shape, plane):
-    '''Draws a cube in a 3D structure.
+    """Draws a cube in a 3D plot.
 
     Notes
     -----
     Originally from:
     https://stackoverflow.com/questions/44881885/python-draw-parallelepiped
-    '''
+    """
     Z = np.array([[0, 0, 0],
                   [1, 0, 0],
                   [1, 1, 0],
@@ -38,7 +40,8 @@ def slice_in_3D(axis, shape, plane):
     r = [-1, 1]
 
     X, Y = np.meshgrid(r, r)
-    # plot vertices
+
+    # plotting vertices
     axis.scatter3D(Z[:, 0], Z[:, 1], Z[:, 2])
 
     # list of sides' polygons of figure
@@ -50,7 +53,7 @@ def slice_in_3D(axis, shape, plane):
              [Z[4], Z[7], Z[3], Z[0]],
              [Z[2], Z[3], Z[7], Z[6]]]
 
-    # plot sides
+    # plotting sides
     axis.add_collection3d(
         Poly3DCollection(verts,
                          facecolors=(0, 1, 1, 0.25),
@@ -76,7 +79,7 @@ def slice_in_3D(axis, shape, plane):
     axis.set_ylabel('col')
     axis.set_zlabel('row')
 
-    # Auto-scale plot axes
+    # auto-scale plot axes
     scaling = np.array([getattr(axis, 'get_{}lim'.format(dim))() for dim in 'xyz'])
     axis.auto_scale_xyz(*[[np.min(scaling), np.max(scaling)]] * 3)
 
@@ -89,24 +92,22 @@ def slice_explorer(data, cmap='gray'):
     @interact(plane=(0, N - 1))
     def display_slice(plane=34):
         fig, axis = plt.subplots(figsize=(20, 5))
-
-        ax_3D = fig.add_subplot(133, projection='3d')
-
-        show_plane(axis, data[plane], title="Plane {}".format(plane), cmap=cmap)
-        slice_in_3D(ax_3D, N, plane)
+        axis_3D = fig.add_subplot(133, projection='3d')
+        show_plane(axis, data[plane], title=f'Plane {plane}', cmap=cmap)
+        slice_in_3D(axis=axis_3D, shape=data.shape, plane=plane)
 
         plt.show()
 
     return display_slice
 
 
-def display(im3d, cmap="gray", step=2):
+def display(data, cmap="gray", step=2):
     _, axes = plt.subplots(nrows=5, ncols=6, figsize=(16, 14))
 
-    vmin = im3d.min()
-    vmax = im3d.max()
+    # getting data min and max to plot limits
+    vmin, vmax = data.min(), data.max()
 
-    for axis, image in zip(axes.flatten(), im3d[::step]):
+    for axis, image in zip(axes.flatten(), data[::step]):
         axis.imshow(image, cmap=cmap, vmin=vmin, vmax=vmax)
         axis.set_xticks([])
         axis.set_yticks([])
@@ -115,8 +116,10 @@ def display(im3d, cmap="gray", step=2):
 
 
 def plot_hist(axis, data, title=None):
+    """Helper function for plotting histograms.
+    """
     axis.hist(data.ravel(), bins=256)
-    axis.ticklabel_format(axis="y", style="scientific", scilimits=(0, 0))
+    axis.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
 
     if title:
         axis.set_title(title)
